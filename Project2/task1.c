d36cfd6104deb8552f6ca3de1e43fe746551359e
hj5230 <hf305@foxmail.com>
2023-04-21 19:22:54 +0300

    commit

---
+++ Project2/task1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define BUFFER_SIZE 5
+
+sem_t empty;
+sem_t full;
+sem_t mutex;
+
+int buffer[BUFFER_SIZE];
+int in = 0;
+int out = 0;
+
+void *producer(void *param) {
+    int product = 0;
+    while (1) {
+        sem_wait(&empty);
+        sem_wait(&mutex);
+
+        buffer[in] = product;
+        printf("Produced: %d\n", product);
+        product++;
+        in = (in + 1) % BUFFER_SIZE;
+
+        sem_post(&mutex);
+        sem_post(&full);
+    }
+}
+
+void *consumer(void *param) {
+    while (1) {
+        sem_wait(&full);
+        sem_wait(&mutex);
+
+        int product = buffer[out];
+        printf("Consumed: %d\n", product);
+        out = (out + 1) % BUFFER_SIZE;
+
+        sem_post(&mutex);
+        sem_post(&empty);
+    }
+}
+
+int main() {
+    pthread_t producer_thread, consumer_thread;
+
+    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+
+    pthread_create(&producer_thread, NULL, producer, NULL);
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
+
+    pthread_join(producer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    sem_destroy(&mutex);
+
+    return 0;
+}
